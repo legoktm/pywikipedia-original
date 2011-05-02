legoktm/pywikipedia-original

commit eeed68105aa43d854820c3fd15c0adc474a4074e

Allow lists of Page and User objects to be interogated
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@9196 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

query.py
@@ -263,10 +263,21 @@ def ListToParam( list ):
 
     encList = ''
     # items may not have one symbol - '|'
-    for l in list:
-        if type(l) == str and u'|' in l:
-            raise wikipedia.Error("item '%s' contains '|' symbol" % l )
-        encList += ToUtf8(l) + u'|'
+    for item in list:
+        if isinstance(item,basestring):
+            if u'|' in item:
+                raise wikipedia.Error(u"item '%s' contains '|' symbol" % item )
+            encList += ToUtf8(item) + u'|'
+        elif isinstance(item,wikipedia.Page):
+            encList += ToUtf8(item.title()) + u'|'
+        elif item.__class__.__name__ == 'User':
+            # delay loading this until it is needed
+            import userlib
+            encList += ToUtf8(item.name()) + u'|'
+        else:
+            raise wikipedia.Error(u'unknown item class %s' % item.__class__.__name__)
+
+    # strip trailing '|' before returning
     return encList[:-1]
 
 def ToUtf8(s):

tests/test_query.py
@@ -7,6 +7,8 @@ __version__ = '$Id: test_userlib.py 9043 2011-03-13 10:25:08Z xqt $'
 import unittest
 import tests.test_pywiki
 
+import wikipedia as pywikibot
+import catlib, userlib
 import query
 
 
@@ -72,6 +74,73 @@ class PyWikiQueryTestCase(tests.test_pywiki.PyWikiTestCase):
             u'registration': u'2007-08-26T02:13:33Z'
         },
         ]}
+        self.assertEqualQueryResult(params, expectedresult)
+
+    def test_titles_Page(self):
+        params = {
+            'action': 'query',
+            'list': 'users',
+            'usprop': ['registration'],
+            'ususers': [pywikibot.Page(self.site, u'Example'),
+                        pywikibot.Page(self.site, u'Example2')],
+        }
+        expectedresult = {u'users': [
+        {
+            u'userid': 215131,
+            u'name': u'Example',
+            u'registration': u'2005-03-19T00:17:19Z'
+        },
+        {
+            u'userid': 5176706,
+            u'name': u'Example2',
+            u'registration': u'2007-08-26T02:13:33Z'
+        },
+        ]}
+        self.assertEqualQueryResult(params, expectedresult)
+
+    def test_titles_User(self):
+        params = {
+            'action': 'query',
+            'list': 'users',
+            'usprop': ['registration'],
+            'ususers': [userlib.User(self.site, u'Example'),
+                        userlib.User(self.site, u'Example2')],
+        }
+        expectedresult = {u'users': [
+        {
+            u'userid': 215131,
+            u'name': u'Example',
+            u'registration': u'2005-03-19T00:17:19Z'
+        },
+        {
+            u'userid': 5176706,
+            u'name': u'Example2',
+            u'registration': u'2007-08-26T02:13:33Z'
+        },
+        ]}
+        self.assertEqualQueryResult(params, expectedresult)
+
+    def test_titles_Category(self):
+        params = {
+            'action': 'query',
+            'prop': 'revisions',
+            'rvprop': ['ids', 'timestamp', 'user'],
+            'rvdir': 'newer',
+            'rvlimit': 1,
+            'titles': [catlib.Category(self.site, u'Category:Categories')],
+        }
+        expectedresult = {u'pages': {u'794823':
+        {
+            u'ns': 14,
+            u'pageid': 794823,
+            u'revisions': [{
+                u'revid': 4494485,
+                u'user': u'SEWilco',
+                u'timestamp': u'2004-07-07T18:45:50Z',
+            }],
+            u'title': u'Category:Categories',
+        },
+        }}
         self.assertEqualQueryResult(params, expectedresult)
 
 if __name__ == "__main__":

commit 2aaf6abdd58c15a6b06bfce4c41057309116465a

enable int type for ListToParam, bugfix for bug #3295288 introduced by r9196
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@9208 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

query.py
@@ -264,18 +264,21 @@ def ListToParam( list ):
     encList = ''
     # items may not have one symbol - '|'
     for item in list:
-        if isinstance(item,basestring):
+        if isinstance(item, basestring):
             if u'|' in item:
-                raise wikipedia.Error(u"item '%s' contains '|' symbol" % item )
+                raise wikipedia.Error(u"item '%s' contains '|' symbol" % item)
             encList += ToUtf8(item) + u'|'
-        elif isinstance(item,wikipedia.Page):
+        if type(item) == 'int':
+            encList += ToUtf8(item) + u'|'
+        elif isinstance(item, wikipedia.Page):
             encList += ToUtf8(item.title()) + u'|'
         elif item.__class__.__name__ == 'User':
             # delay loading this until it is needed
             import userlib
             encList += ToUtf8(item.name()) + u'|'
         else:
-            raise wikipedia.Error(u'unknown item class %s' % item.__class__.__name__)
+            raise wikipedia.Error(u'unknown item class %s'
+                                  % item.__class__.__name__)
 
     # strip trailing '|' before returning
     return encList[:-1]