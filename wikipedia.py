59a6be60a3194623494b81323b4f07ae2922b97b
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-05-24 14:05:21 +0000

    lastRev['*'] may be False. Test it.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10247 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -781,7 +781,7 @@ not supported by PyWikipediaBot!"""
                 raise BadTitle('BadTitle: %s' % self)
         elif 'revisions' in pageInfo: #valid Title
             lastRev = pageInfo['revisions'][0]
-            if lastRev['*'] is not None:
+            if lastRev['*']:
                 textareaFound = True
         # I got page date with 'revisions' in pageInfo but
         # lastRev['*'] = False instead of the content. The Page itself was
