b115f7717251c39da0e467d6919b5c3f02958154
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-06-21 05:58:48 +0000

    revert r10398
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10399 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- flickrripper.py
+++ flickrripper.py
@@ -156,8 +156,7 @@ def getFilename(photoInfo=None, site=None, project=u'Flickr'):
         #find the max length for a mw title
         maxBytes = 240 - len(project.encode('utf-8')) \
                        - len(username.encode('utf-8'))
-        description = pywikibot.html2unicode(
-            photoInfo.find('photo').find('description').text)
+        description = photoInfo.find('photo').find('description').text
         if description:
             descBytes = len(description.encode('utf-8'))
             if descBytes > maxBytes:
