7376345d9dbd0d5b488ca72b787f8277c3c6a2db
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-06-21 06:04:53 +0000

    fix for r10387
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10400 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- flickrripper.py
+++ flickrripper.py
@@ -161,7 +161,7 @@ def getFilename(photoInfo=None, site=None, project=u'Flickr'):
             descBytes = len(description.encode('utf-8'))
             if descBytes > maxBytes:
                 # maybe we cut more than needed, anyway we do it
-                items = max(0, len(description) - maxBytes + descBytes)
+                items = max(0, len(description) - descBytes + maxBytes)
                 description = description[:items]
             title = cleanUpTitle(description)
         else:
