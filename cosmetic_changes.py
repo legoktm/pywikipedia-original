6441bf1473c3e7067da4e016e5ccd3fc7fc49f46
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-06-28 06:55:52 +0000

    invalid replacements inside file links; fix for bug #3538506
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10425 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- cosmetic_changes.py
+++ cosmetic_changes.py
@@ -208,6 +208,7 @@ class CosmeticChangesToolkit:
         old instances standardizeInterwiki and standardizeCategories
         The page footer has the following section in that sequence:
         1. categories
+        2a TODO:template beyond categories
         2. additional information depending on local site policy
         3. stars templates for featured and good articles
         4. interwiki links
@@ -729,8 +730,8 @@ class CosmeticChangesToolkit:
         # This only works if there are only two items in digits dict
         old = digits[digits.keys()[0]]
         # do not change inside file links
-        namespaces = list(self.site.namespace(6, all = True))
-        pattern = re.compile(u'\[\[(' + '|'.join(namespaces) + '):.+?\..+?\]\]',
+        namespaces = list(self.site.namespace(6, all=True))
+        pattern = re.compile(u'\[\[(' + '|'.join(namespaces) + '):.+?\.\w+? *(\|[^\[]*?(\[\[[^\[]*?\]\][^\[]*?)?)?\]\]',
                              re.UNICODE)
         exceptions.append(pattern)
         text = pywikibot.replaceExcept(text, u',', u'،', exceptions)
@@ -742,8 +743,8 @@ class CosmeticChangesToolkit:
             text = pywikibot.replaceExcept(text, u'ه', u'ھ', exceptions)
         text = pywikibot.replaceExcept(text, u'ك', u'ک', exceptions)
         text = pywikibot.replaceExcept(text, ur'[ىي]', u'ی', exceptions)
-        # replace persian digits
-        for i in range(0,10):
+        # replace persian/arabic digits
+        for i in xrange(0,10):
             text = pywikibot.replaceExcept(text, old[i], new[i], exceptions)
         # do not change digits in class, style and table params
         pattern = re.compile(u'\w+=(".+?"|\d+)', re.UNICODE)
@@ -752,13 +753,14 @@ class CosmeticChangesToolkit:
         pattern = re.compile(u'<[/]*?[^</]+?[/]*?>', re.UNICODE)
         exceptions.append(pattern)
         exceptions.append('table') #exclude tables for now
+        # replace digits
+        for i in xrange(0, 10):
+            text = pywikibot.replaceExcept(text, str(i), new[i], exceptions)
         ##fixing pipe and trailing for fa. Thanks ZxxZxxZ
         if self.site.lang=='fa':
             faChrs = u'ءاآأإئؤبپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیةيك' + u'ًٌٍَُِّْٓٔ'
             text = re.sub(u'\[\[([^\]\|]*)]]([‌%s]+)' % faChrs, ur'[[\1|\1\2]]', text)
             text = re.sub(u'\[\[([^\]\|]*)\|(.+?)]]([‌%s]+)' % faChrs, ur'[[\1|\2\3]]', text)
-        for i in range(0,10):
-            text = pywikibot.replaceExcept(text, str(i), new[i], exceptions)
         return text
 
     # Retrieved from "http://commons.wikimedia.org/wiki/Commons:Tools/pywiki_file_description_cleanup"
