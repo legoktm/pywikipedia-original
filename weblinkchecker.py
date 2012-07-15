d754367ad901ab5b426acf8ae725abd8abbd328e
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-07-15 08:27:45 +0000

    exclusion of pages rejecting bots, patch submitted by masti, bug #3544207. thanks
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10457 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- weblinkchecker.py
+++ weblinkchecker.py
@@ -215,6 +215,8 @@ ignorelist = [
     re.compile('.*[\./@]itis\.gov(/.*)?'), # bot rejected on the site
     re.compile('.*[\./@]cev\.lu(/.*)?'), # bot rejected on the site
     re.compile('.*[\./@]science\.ksc\.nasa\.gov(/.*)?'), # very slow response resulting in bot error
+    re.compile('.*[\./@]britannica\.com(/.*)?'), #HTTP redirect loop
+    re.compile('.*[\./@]quickfacts\.census\.gov(/.*)?'), # bot rejected on the site
 ]
 
 def weblinksIn(text, withoutBracketed = False, onlyBracketed = False):
