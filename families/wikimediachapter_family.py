2821b5f96c5ac59061409209273e1b031b61fe8d
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-08-30 20:40:38 +0000

    Some parts are inherited from family.Family; SVN properties added; some PEP8 designs
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10510 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/wikimediachapter_family.py
+++ families/wikimediachapter_family.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+__version__ = '$Id$'
 
 # The wikis of Chapters of the Wikimedia Foundation living at a xy.wikimedia.org url
 
@@ -9,14 +10,38 @@ class Family(family.Family):
     def __init__(self):
         family.Family.__init__(self)
         self.name = 'wikimediachapter'
-        self.countries = [ 'ar', 'bd', 'co', 'dk', 'fi', 'mk', 'mx', 'nl', 'no', 'nyc', 'pl', 'rs', 'ru', 'se', 'ua', 'uk', 've' ]
-        self.countrylangs = { 'ar': 'es', 'bd': 'bn', 'co': 'es', 'dk': 'da', 'fi': 'fi', 'mk': 'mk', 'mx': 'es', 'nl': 'nl', 'no': 'no', 'nyc': 'en', 'pl': 'pl', 'rs': 'sr', 'ru': 'ru', 'se': 'sv', 'ua': 'uk', 'uk': 'en-gb', 've': 'en' }
-        self.langs = dict([(country, '%s.wikimedia.org' % country) for country in self.countries])
+
+        self.countries = [
+            'ar', 'bd', 'co', 'dk', 'fi', 'mk', 'mx', 'nl', 'no', 'nyc', 'pl',
+            'rs', 'ru', 'se', 'ua', 'uk', 've',
+        ]
+
+        self.countrylangs = {
+            'ar': 'es', 'bd': 'bn', 'co': 'es', 'dk': 'da', 'fi': 'fi',
+            'mk': 'mk', 'mx': 'es', 'nl': 'nl', 'no': 'no', 'nyc': 'en',
+            'pl': 'pl', 'rs': 'sr', 'ru': 'ru', 'se': 'sv', 'ua': 'uk',
+            'uk': 'en-gb', 've': 'en',
+        }
+
+        self.langs = dict([(country, '%s.wikimedia.org' % country)
+                           for country in self.countries])
 
         for country in self.countries:
             for ns in self.namespaces:
-                self.namespaces[ns][country] = self.namespaces[ns][self.countrylangs[country]] if self.countrylangs[country] in self.namespaces[ns] else self.namespaces[ns]['_default']
+                self.namespaces[ns][country] = self.namespaces[ns][self.countrylangs[country]] \
+                                               if self.countrylangs[country] in self.namespaces[ns] \
+                                               else self.namespaces[ns]['_default']
 
+        # Override defaults
+        self.namespaces[9]['dk'] = u'MediaWiki diskussion'
+        self.namespaces[13]['dk'] = u'Hjælp diskussion'
+        self.namespaces[2]['pl'] = u'Użytkownik'
+        self.namespaces[3]['pl'] = u'Dyskusja użytkownika'
+
+        # Most namespaces are inherited from family.Family.
+        # Translation used on all wikis for the different namespaces.
+        # (Please sort languages alphabetically)
+        # You only need to enter translations that differ from _default.
         self.namespaces[4] = {
             '_default': 'Wikimedia',
             'bd': u'উইকিমিডিয়া বাংলাদেশ',
@@ -46,22 +71,33 @@ class Family(family.Family):
             'fi': u'Keskustelu Wikimediasta'
         }
 
-        self.namespaces[9]['dk'] = u'MediaWiki diskussion'
-        self.namespaces[13]['dk'] = u'Hjælp diskussion'
+        self.namespaces[90] = {
+            'fi': u'Viestiketju',
+            'se': u'Tråd',
+        }
 
-        self.namespaces[2]['pl'] = u'Użytkownik'
-        self.namespaces[3]['pl'] = u'Dyskusja użytkownika'
+        self.namespaces[91] = {
+            'fi': u'Keskustelu viestiketjusta',
+            'se': u'Tråddiskussion',
+        }
 
-        self.namespaces[90] = { 'fi': u'Viestiketju', 'se': u'Tråd' }
-        self.namespaces[91] = { 'fi': u'Keskustelu viestiketjusta', 'se': u'Tråddiskussion' }
-        self.namespaces[92] = { 'fi': u'Yhteenveto', 'se': u'Summering' }
-        self.namespaces[93] = { 'fi': u'Keskustelu yhteenvedosta', 'se': u'Summeringsdiskussion' }
-        self.namespaces[100] = { 'nl': u'De Wikiaan', 'se': u'Projekt' }
-        self.namespaces[101] = { 'nl': u'Overleg De Wikiaan', 'se': u'Projektdiskussion' }
+        self.namespaces[92] = {
+            'fi': u'Yhteenveto',
+            'se': u'Summering',
+        }
 
-    def scriptpath(self, code):
-        return '/w'
+        self.namespaces[93] = {
+            'fi': u'Keskustelu yhteenvedosta',
+            'se': u'Summeringsdiskussion',
+        }
 
-    def version(self, code):
-        return u'1.20wmf9'
+        self.namespaces[100] = {
+            'nl': u'De Wikiaan',
+            'se': u'Projekt',
+        }
+
+        self.namespaces[101] = {
+            'nl': u'Overleg De Wikiaan',
+            'se': u'Projektdiskussion',
+        }
 
