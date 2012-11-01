legoktm/pywikipedia-original

commit a49d976122acb65f794650b2acb0cdc39401804d

edit restriction for some templates on zh-wiki where interlanguage keys are included in /doc sub-page
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10629 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

interwiki.py
@@ -1654,14 +1654,21 @@ u'WARNING: %s is in namespace %i, but %s is in namespace %i. Follow it anyway?'
                             break
         else:
             for (site, page) in new.iteritems():
+                # edit restriction for some templates on zh-wiki where interlanguage keys are included
+                # by /doc subpage
+                smallWikiAllowed = not (page.site.sitename() == 'wikipedia:zh' and
+                                        page.site.namespace() == 10 and
+                                        u'Country data' in page.title(withNamespace=False))
                 # edit restriction on is-wiki
                 # http://is.wikipedia.org/wiki/Wikipediaspjall:V%C3%A9lmenni
+                # and zh-wiki for template namespace which prevents increasing the queue
                 # allow edits for the same conditions as -whenneeded
                 # or the last edit wasn't a bot
                 # or the last edit was 1 month ago
-                smallWikiAllowed = True
-                if globalvar.autonomous and (page.site.sitename() == 'wikipedia:is' or
-                                             page.site.sitename() == 'wikipedia:zh'):
+                if smallWikiAllowed and globalvar.autonomous and \
+                   (page.site.sitename() == 'wikipedia:is' or
+                    page.site.sitename() == 'wikipedia:zh' and
+                    page.site.namespace() == 10):
                     old={}
                     try:
                         for mypage in new[page.site].interwiki():

commit 4c882e07f10185bf81b40644bcfceb35e7f0d3aa

bugfix for bug #3582353 introduced in r10629
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10636 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

interwiki.py
@@ -1674,7 +1674,7 @@ u'WARNING: %s is in namespace %i, but %s is in namespace %i. Follow it anyway?'
                 if smallWikiAllowed and globalvar.autonomous and \
                    (page.site.sitename() == 'wikipedia:is' or
                     page.site.sitename() == 'wikipedia:zh' and
-                    page.site.namespace() == 10):
+                    page.namespace() == 10):
                     old={}
                     try:
                         for mypage in new[page.site].interwiki():