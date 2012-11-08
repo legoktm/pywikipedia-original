c3e61a769e6a6a9b2bbb4a4f5726573f0e56ce44
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2012-11-08 14:05:25 +0000

    mw 1.20wmf3, update from rewrite r10679
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10680 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- family.py
+++ family.py
@@ -4076,7 +4076,7 @@ class Family:
         """Return MediaWiki version number as a string."""
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
-        return '1.21wmf2'
+        return '1.21wmf3'
 
     def versionnumber(self, code, version=None):
         """Return an int identifying MediaWiki version.
