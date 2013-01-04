09939cc8eeaaa2db37d63c18e98679b1897e1123
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2013-01-04 12:57:56 +0000

    nowcommons.py patch for ru.wiki submitted by rubin16 (patch bug #3599460)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10871 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- nowcommons.py
+++ nowcommons.py
@@ -48,8 +48,8 @@ Please fix these if you are capable and motivated:
 #
 # (C) Wikipedian, 2006-2007
 # (C) Siebrand Mazeland, 2007-2008
-# (C) xqt,2010-2012
-# (C) Pywikipedia bot team, 2006-2011
+# (C) xqt, 2010-2012
+# (C) Pywikipedia bot team, 2006-2013
 #
 # Distributed under the terms of the MIT license.
 #
@@ -150,6 +150,7 @@ nowCommons = {
         u'Now Commons',
         u'Db-commons',
         u'Перенесено на Викисклад',
+        u'На Викискладе',
     ],
     'zh':[
         u'NowCommons',
