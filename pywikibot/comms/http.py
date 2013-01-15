a792c0cfc7fe82e4b2d755f08fccffae028a4e34
drtrigon <drtrigon@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2013-01-15 21:07:16 +0000

    follow-up; to r10930 in order to bug fix
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10934 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pywikibot/comms/http.py
+++ pywikibot/comms/http.py
@@ -212,7 +212,7 @@ u"WARNING: Could not open '%s'. Maybe the server or\n your connection is down. R
         text = unicode(text, charset, errors = 'strict')
     except UnicodeDecodeError, e:
         if verbose:
-            output(u'%s' %e)
+            pywikibot.output(u'%s' %e)
         if no_hostname:
             pywikibot.output(u'ERROR: Invalid characters found on %s, replaced by \\ufffd.' % uri)
         else:
