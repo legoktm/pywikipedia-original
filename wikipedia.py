35d8ed193753d37c40cb9f409fdd26dfa10432ee
multichill <multichill@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2013-01-23 21:32:04 +0000

    def delete now adds a {{speedydelete|1=<reason>|2=bot}} instead of {{delete|bot=yes}}
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@10976 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -3465,7 +3465,8 @@ u'Page %s is semi-protected. Getting edit page to find out if we are allowed to 
                  output(u'Cannot delete page %s - marking the page for deletion instead:' % self.title(asLink=True))
                  # Note: Parameters to {{delete}}, and their meanings, vary from one Wikipedia to another.
                  # If you want or need to use them, you must be careful not to break others. Else don't.
-                 self.put(u'{{delete|bot=yes}}\n%s --~~~~\n----\n\n%s' % (reason, text), comment = reason)
+                 # FIXME: Make some sort of configuration setting to keep track of the templates in different wiki's
+                 self.put(u'{{speedydelete|1=%s --~~~~|bot=yes}}\n\n%s' % (reason, text), comment = reason)
                  return
              else:
                  raise
